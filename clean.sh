90941db8fdaf8cffd89edea8bd000fb15ecc0db7
Nikita <nikich340@gmail.com>
2015-10-02 21:18:36 +1000

    updates

--- clean.sh
+++ clean.sh
@@ -8,13 +8,9 @@ rm ./RAMDISKs/D620-V20C/split_img/zImage
 rm ./RAMDISKs/D620-V20C/split_img/dt.img
 rm ./RAMDISKs/D620-V20C/system/lib/modules/*.ko
 # D410
-rm ./RAMDISKs/D410-V20C/split_img/zImage
-rm ./RAMDISKs/D410-V20C/split_img/dt.img
-rm ./RAMDISKs/D410-V20C/system/lib/modules/*.ko
-# D405
-rm ./RAMDISKs/D405-V20A/split_img/zImage
-rm ./RAMDISKs/D405-V20A/split_img/dt.img
-rm ./RAMDISKs/D405-V20A/system/lib/modules/*.ko
+rm ./RAMDISKs/D410-V20D/split_img/zImage
+rm ./RAMDISKs/D410-V20D/split_img/dt.img
+rm ./RAMDISKs/D410-V20D/system/lib/modules/*.ko
 # D405n
 rm ./RAMDISKs/D405n-V20A/split_img/zImage
 rm ./RAMDISKs/D405n-V20A/split_img/dt.img
